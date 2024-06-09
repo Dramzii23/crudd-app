b37b796d7bbd5f5a2e78ab29d6dcb746303e16f3
Dramzii <dramzii23@gmail.com>
2024-06-09 10:16:38 -0600

    First CRUD App Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  const [editDescription, setEditDescription] = useState("");
+  const [editPrecio, setEditPrecio] = useState("");
+
+  useEffect(() => {
+    traerItems();
+  }, []);
+
+  const traerItems = async () => {
+    const response = await fetch("http://localhost:3001/api/traeritem");
+    const data = await response.json(); // data es un array de objetos
+    setItems(data);
+  };
+const agregarItem = async () => {
+    const response = await.fetch("http://localhost:3001/api/agregaritem", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+                            descripcion: cuerpoRequest.descripcion,
+                            precio: cuerpoRequest.precio
+                          })
+
+                          
+    });
+
   return (
     <div className="App">
       <header className="App-header">
