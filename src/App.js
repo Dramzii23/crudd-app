c21f2b9f594894ae4402a904e2c83a3eb6e24c52
Dramzii <dramzii23@gmail.com>
2024-06-10 20:37:13 -0600

    Traer items funciona pero hay que hacer refresh

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
-  const [editDescription, setEditDescription] = useState("");
+  const [newDescripcion, setnewDescripcion] = useState("");
+  const [newPrecio, setNewPrecio] = useState(null);
+  const [editDescripcion, setEditDescripcion] = useState(null);
   const [editPrecio, setEditPrecio] = useState("");
 
   useEffect(() => {
@@ -14,40 +15,58 @@ function App() {
   }, []);
 
   const traerItems = async () => {
-    const response = await fetch("http://localhost:3001/api/traeritem");
+    const response = await fetch("http://localhost:3001/api/traeritems");
     const data = await response.json(); // data es un array de objetos
     setItems(data);
   };
-const agregarItem = async () => {
-    const response = await.fetch("http://localhost:3001/api/agregaritem", {
+  const agregarItem = async () => {
+    const response = await fetch("http://localhost:3001/api/agregaritem", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-                            descripcion: cuerpoRequest.descripcion,
-                            precio: cuerpoRequest.precio
-                          })
-
-                          
+        descripcion: newDescripcion,
+        precio: newPrecio,
+      }),
     });
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>CRUD BASICO DE ITEMS</h1>
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>Descripcion: </label>
+          <input
+            type="text"
+            value={newDescripcion}
+            onChange={(e) => setnewDescripcion(e.target.value)}
+            placeholder="Ingrese una descripcion"
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <label>Precio: </label>
+          <input
+            type="number"
+            value={newPrecio}
+            onChange={(e) => setNewPrecio(e.target.value)}
+            placeholder="Ingrese un precio"
+          />
+        </p>
+        <button onClick={agregarItem}>Agregar Item</button>
+      </div>
+      <div>
+        <h2>Items</h2>
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              <b>Decripción: </b>
+              {item.descripcion} - <b>Precio: </b> {item.precio}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
